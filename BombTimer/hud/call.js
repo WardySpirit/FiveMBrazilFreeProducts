225f1c5e1ab27e259beb730b491496bac6d00ac2
WardySpirit <wardyguardian@gmail.com>
2020-07-02 22:25:06 -0300

    HUD Releases

--- BombTimer/hud/call.js
+++ BombTimer/hud/call.js
@@ -1,29 +1,4 @@
-/*
-    ███████████ ██████████ ██████   █████ ███████████   █████ ███████████  
-    ░░███░░░░░░█░░███░░░░░█░░██████ ░░███ ░░███░░░░░███ ░░███ ░░███░░░░░███ 
-     ░███   █ ░  ░███  █ ░  ░███░███ ░███  ░███    ░███  ░███  ░███    ░███ 
-     ░███████    ░██████    ░███░░███░███  ░██████████   ░███  ░██████████  
-     ░███░░░█    ░███░░█    ░███ ░░██████  ░███░░░░░███  ░███  ░███░░░░░███ 
-     ░███  ░     ░███ ░   █ ░███  ░░█████  ░███    ░███  ░███  ░███    ░███ 
-     █████       ██████████ █████  ░░█████ █████   █████ █████ █████   █████
-     ░░░░░       ░░░░░░░░░░ ░░░░░    ░░░░░ ░░░░░   ░░░░░ ░░░░░ ░░░░░   ░░░░░ 
-                                                                        
-    Meu Discord: Fenrir#9143
 
-    Meu Servidor: https://discord.gg/KkCBwsQ
-
-    [charity] Obrigado por comprar o meu servico! 5% do dinheiro gasto nesse servico sera usado no 
-    final do mes para ajudar animais silvestres em regioes de caça esportiva.
-
-    O script ainda esta na versao BETA, oq significa que eh possivel encontrar bugs
-    enquanto ultiliza-lo, principalmente em outras Frameworks como ESX e vRP.
-
-    Caso voce encontre algum bug ou deseje dar alguma dica para aprima-lo, avise-me
-    em meu discord. Seu nome sera creditado no proximo Changelog caso tal aprimoramento
-    seja adicionado ou o bug seja resolvido <3.
-
-    Caso queira, voce ja pode deletar esse comentario para limpar visualmente o script.
-*/
 $(function(){
     window.onload = function(e){
         $('.container').hide();
